e5f4061ddfdd9313e4bc84ba5a867ba86041f416
SeungwooDY <sdyoon0701@gmail.com>
2026-05-15 16:21:41 -0400

    auth0 progress

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import express , { Request, Response } from 'express';
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
-import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+import { NodeStreamableHTTPServerTransport } from '@modelcontextprotocol/node';
 import { isInitializeRequest } from '@modelcontextprotocol/sdk/types.js';
 import { randomUUID } from 'node:crypto';
 import { auth } from 'express-oauth2-jwt-bearer';
 import { createServer } from './server.js';
 
-const app = express();
-app.use(express.json());
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4101;
+const app = express(); // creates express application
+app.use(express.json()); // tell express to automatically parse incoming request bodies as JSON
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4101; // read port from environment, otherwise fall back to 4101
 
+// create auth0 middleware instance
 const checkJwt = auth({
     audience: process.env.AUTH0_AUDIENCE!,
     issuerBaseURL: `https://${process.env.AUTH0_DOMAIN!}/`,
 });
 
-app.get('./well-known/oauth-protected-resource', (req, res) => {
+// store transport instances in-memory (just for demo)
+const transports: Record<string, NodeStreamableHTTPServerTransport> = {};
+
+app.get('/.well-known/oauth-protected-resource', (req, res) => {
     res.json({
         resource: process.env.RESOURCE_URL!,
         authorization_servers: [`https://${process.env.AUTH0_DOMAIN!}`],
@@ -23,3 +27,39 @@ app.get('./well-known/oauth-protected-resource', (req, res) => {
         bearer_methods_supported: ['header'],
     });
 });
+
+// checkJwt runs before handler (makes sure bearer token is valid)
+app.post('/mcp', checkJwt, async (req, res) => {
+    const sessionId = req.headers['mcp-session-id'] as string | undefined;
+    let transport: NodeStreamableHTTPServerTransport;
+
+    // if client already sent a session ID and it exists in the store, reuse transport you created for them
+    if (sessionId && transports[sessionId]) {
+        transport = transports[sessionId];
+    } else if (!sessionId && isInitializeRequest(req.body)) {
+
+        // if sessionID doesn't exist (new client), create new transport with random UUID
+        transport = new NodeStreamableHTTPServerTransport({
+            sessionIdGenerator: () => randomUUID(),
+        });
+
+        // create new MCP server instance
+        const server = createServer();
+
+        // connect server to the transport
+        await server.connect(transport);
+
+        // store the transport in session map
+        transports[transport.sessionId!] = transport;
+
+        // delete transport if transport closes
+        transport.onclose = () => {
+            delete transports[transport.sessionId!];
+        };
+    } else {
+        res.status(400).json({ error: 'Invalid request' });
+        return;
+    }
+
+    await transport.handleRequest(req as any, res as any, req.body);
+})
